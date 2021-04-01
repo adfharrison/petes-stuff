759c88cfd6e8694c7401cda838ae96f6785c3c7d
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2021-04-01 16:41:58 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Axios from 'axios';
 
+// set up base api request url
+const crossoverApi = Axios.create({
+  baseURL: 'https://petes-api.com',
+});
+
+// create app component
+const App = () => {
+  // create a bit of state to store xover data (we will assume its just a string)
+  const [xovers, setXovers] = useState(null);
+
+  // make request to api on click of button
+  const getUsers = async () => {
+    const { data } = await crossoverApi.get('/crossovers');
+    // assuming this api endpoint gives a JSON object with a property of data.xovers
+    setXovers(data.xovers);
+  };
+
+  // what to render
+  return (
+    <div>
+      <h1>Petes App</h1>
+      <button onClick={getUsers}>Get Xovers</button>
+      {/* do we have the xovers? if so, render them, if not, a string */}
+      <p>{xovers ? xovers : 'get xovers to show xovers'}</p>
+    </div>
+  );
+};
+
+// render app in DOM (see index.html in public folder)
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
